b17e40912c4c40321eb77f98890aba1c923bc425
Moballighul islam <moballigh5225@gmail.com>
2023-10-08 02:03:57 +0530

    day-2 commit

---
+++ day-2/index.js
@@ -0,0 +1,10 @@
+// TODO 1: WRITE A PROGRAM TO ADD TWO NUMBERS?
+// TODO 2: HOW TO Catch Errors & solve it?
+// TODO 3: TS Configuration File
+//* fun define
+// @ts-ignore
+function sum(a, b) {
+    return a + b;
+}
+// call func
+console.log(sum(5, 10));
